7550137e01ec2d76063ff8bd8740fade24f3600b
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-07-08 14:02:50 -0500

    Add DTO datos usuario

--- API/src/main/java/alura/com/api/domain/usuario/DatosUsuarioDTO.java
+++ API/src/main/java/alura/com/api/domain/usuario/DatosUsuarioDTO.java
@@ -2,7 +2,7 @@ package alura.com.api.domain.usuario;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record DatosAutorDTO(
+public record DatosUsuarioDTO(
         @NotBlank
         String nombre
 ) {
