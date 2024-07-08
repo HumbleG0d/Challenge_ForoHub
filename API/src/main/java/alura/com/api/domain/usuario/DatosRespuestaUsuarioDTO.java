af2b0bc2f3a2e2b8d76758bc0504b2ec9e439002
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-07-08 14:03:14 -0500

    Add DTO datos respuesta

--- API/src/main/java/alura/com/api/domain/usuario/DatosRespuestaUsuarioDTO.java
+++ API/src/main/java/alura/com/api/domain/usuario/DatosRespuestaUsuarioDTO.java
@@ -1,2 +1,15 @@
-package alura.com.api.domain.usuario;public record DatosRespuestaUsuarioDTO() {
+package alura.com.api.domain.usuario;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record DatosRespuestaUsuarioDTO(
+        @NotNull
+        Long id,
+        @NotBlank
+        String nombre
+) {
+  public DatosRespuestaUsuarioDTO (Usuario usuario) {
+   this(usuario.getId(), usuario.getNombre());
+  }
 }
