9a8c520cc2b6ac8fc2b2a7c42d7fb7b2b1386312
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-07-08 13:53:56 -0500

    Add DTO curso

--- API/src/main/java/alura/com/api/domain/curso/DatosCursoDTO.java
+++ API/src/main/java/alura/com/api/domain/curso/DatosCursoDTO.java
@@ -1,2 +1,11 @@
-package alura.com.api.domain.curso;public record DatosCursoDTO() {
+package alura.com.api.domain.curso;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record DatosCursoDTO(
+        @NotBlank
+        String nombre,
+        @NotBlank
+        String categoria
+) {
 }
