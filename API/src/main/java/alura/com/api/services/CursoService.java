15eee9cadbb4a22c4ce28996844898fe2e8f1573
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-07-08 16:21:06 -0500

    Add servicio curso

--- API/src/main/java/alura/com/api/services/CursoService.java
+++ API/src/main/java/alura/com/api/services/CursoService.java
@@ -1,2 +1,16 @@
-package alura.com.api.services;public class CursoService {
+package alura.com.api.services;
+
+import alura.com.api.domain.curso.Curso;
+import alura.com.api.repository.CursoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CursoService {
+  @Autowired
+  private CursoRepository cursoRepository;
+
+  public Curso save(Curso curso){
+    return cursoRepository.save(curso);
+  }
 }
