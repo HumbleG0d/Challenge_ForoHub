1c1b5d170ce100852004af0cb66b434cd68d2839
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-07-08 16:15:08 -0500

    Add datos actualizados

--- API/src/main/java/alura/com/api/domain/topico/DatosActualizarTopicoDTO.java
+++ API/src/main/java/alura/com/api/domain/topico/DatosActualizarTopicoDTO.java
@@ -1,2 +1,16 @@
-package alura.com.api.domain.topico;public record DatosActualizarTopicoDTO() {
+package alura.com.api.domain.topico;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record DatosActualizarTopicoDTO(
+        @NotNull
+        Long id,
+
+        @NotBlank
+        String mensaje,
+
+        @NotBlank
+        String status
+) {
 }
