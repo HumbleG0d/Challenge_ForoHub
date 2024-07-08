85051867448bf81aaf23fa571b0331fdc4b3e006
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-07-08 13:57:33 -0500

    Add DTO respuesta topico

--- API/src/main/java/alura/com/api/domain/topico/DatosRespuestaTopicoDTO.java
+++ API/src/main/java/alura/com/api/domain/topico/DatosRespuestaTopicoDTO.java
@@ -1,2 +1,18 @@
-package alura.com.api.domain.topico;public record DatosRespuestaTopicoDTO() {
+package alura.com.api.domain.topico;
+
+import alura.com.api.domain.curso.Curso;
+import alura.com.api.domain.usuario.Usuario;
+
+public record DatosRespuestaTopicoDTO(
+        Long id,
+        String titulo,
+        String mensaje,
+        String fecha_creacion,
+        String status,
+        Usuario usuario,
+        Curso curso
+) {
+  public DatosRespuestaTopicoDTO(Topico topico) {
+    this(topico.getId(),topico.getTitulo(),topico.getMensaje() ,topico.getFecha_creacion().toString() , topico.getStatus().toString() , topico.getUsuario() ,topico.getCurso());
+  }
 }
