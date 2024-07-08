0ea972217d5475b9287b483c6f1b73bf7a2e7a48
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-07-08 16:20:34 -0500

    Add usuario controller

--- API/src/main/java/alura/com/api/controller/UsuarioController.java
+++ API/src/main/java/alura/com/api/controller/UsuarioController.java
@@ -1,2 +1,41 @@
-package alura.com.api.controller;public class UsuarioController {
+package alura.com.api.controller;
+
+import alura.com.api.domain.usuario.DatosRegistroUsuariosDTO;
+import alura.com.api.domain.usuario.DatosRespuestaUsuarioDTO;
+import alura.com.api.domain.usuario.Usuario;
+import alura.com.api.repository.UsuarioRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+  @Autowired
+  private UsuarioRepository usuarioRepository;
+
+  @PostMapping
+  public ResponseEntity<DatosRespuestaUsuarioDTO> agregarUsuario(@RequestBody @Valid DatosRegistroUsuariosDTO datosRegistroUsuariosDTO , UriComponentsBuilder uriComponentsBuilder){
+  Usuario usuario =usuarioRepository.save(new Usuario(datosRegistroUsuariosDTO));
+  DatosRespuestaUsuarioDTO datosRespuestaUsuarioDTO = new DatosRespuestaUsuarioDTO(usuario);
+  URI url = uriComponentsBuilder.path("/usuario/{id}").buildAndExpand(usuario.getId()).toUri();
+  return ResponseEntity.created(url).body(datosRespuestaUsuarioDTO);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<DatosRespuestaUsuarioDTO> buscarUsuario(@PathVariable Long id){
+    Optional<Usuario> usuarioOptional = usuarioRepository.findById(id);
+    if(usuarioOptional.isEmpty()){
+      return ResponseEntity.notFound().build();
+    }
+    DatosRespuestaUsuarioDTO datosRespuestaUsuarioDTO = new DatosRespuestaUsuarioDTO(usuarioOptional.get());
+    return ResponseEntity.ok(datosRespuestaUsuarioDTO);
+  }
+
 }
