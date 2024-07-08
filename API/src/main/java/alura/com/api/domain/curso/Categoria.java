19ce3ab1f3c24fa06779f4223c3f590cf85f66d5
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-07-08 13:52:55 -0500

    Add the cotheogy of the curses

--- API/src/main/java/alura/com/api/domain/curso/Categoria.java
+++ API/src/main/java/alura/com/api/domain/curso/Categoria.java
@@ -1,2 +1,9 @@
-package alura.com.api.domain.curso;public enum Categoria {
+package alura.com.api.domain.curso;
+
+public enum Categoria {
+  DESARROLLO_MOVIL,
+  DESARROLLO_WEB,
+  CIENCIA_DATOS,
+  CIBERSEGURIDAD,
+  DEVOPS;
 }
