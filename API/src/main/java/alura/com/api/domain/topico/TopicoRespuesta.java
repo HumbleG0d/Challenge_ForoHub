1435b98f7cd331ef7132df4fd8130364abbb540c
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-07-08 13:56:29 -0500

    Add class TopicoRespuesta

--- API/src/main/java/alura/com/api/domain/topico/TopicoRespuesta.java
+++ API/src/main/java/alura/com/api/domain/topico/TopicoRespuesta.java
@@ -1,5 +1,19 @@
 package alura.com.api.domain.topico;
 
-public class Topico_respuesta {
-  
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "TopicoRespuesta")
+@Table(name = "topico_respuestas")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class TopicoRespuesta {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private Long topico_id;
+  private Long respuesta_id;
 }
