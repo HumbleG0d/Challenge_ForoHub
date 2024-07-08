a6ef303ddbc692ddd0d13c7036282e839efdcb73
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-07-08 16:19:45 -0500

    Add repositorio topico

--- API/src/main/java/alura/com/api/repository/TopicoRepository.java
+++ API/src/main/java/alura/com/api/repository/TopicoRepository.java
@@ -1,2 +1,20 @@
-package alura.com.api.repository;public interface TopicoRepository {
+package alura.com.api.repository;
+import alura.com.api.domain.topico.Topico;
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+  @Query("SELECT t FROM Topico t")
+  Page<Topico> getTopicos(Pageable pageable);
+
+  @Modifying
+  @Transactional
+  @Query("DELETE FROM Topico t WHERE t.id = :id")
+  void deleteTopicoByid(Long id);
 }
