2d6f804fd9669cb57cae0c3331e09ff6ab0e7624
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-07-08 13:56:57 -0500

    Add DTO datos topico

--- API/src/main/java/alura/com/api/domain/topico/DatosTopicoDTO.java
+++ API/src/main/java/alura/com/api/domain/topico/DatosTopicoDTO.java
@@ -1,2 +1,22 @@
-package alura.com.api.domain.topico;public record DatosTopicoDTO() {
+package alura.com.api.domain.topico;
+
+import alura.com.api.domain.curso.DatosCursoDTO;
+import alura.com.api.domain.usuario.DatosUsuarioDTO;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record DatosTopicoDTO(
+        @NotBlank
+        String titulo,
+        @NotBlank
+        String mensaje,
+        @NotBlank
+        String fecha_creacion,
+        @NotBlank
+        String status,
+        @NotNull
+        DatosUsuarioDTO usuario,
+        @NotNull
+        DatosCursoDTO curso
+) {
 }
