4d24203ba3dd18ae7efa46fc1e2056f3c63af238
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-07-08 13:54:38 -0500

    Add ckass Respuesta

--- API/src/main/java/alura/com/api/domain/respuesta/Respuesta.java
+++ API/src/main/java/alura/com/api/domain/respuesta/Respuesta.java
@@ -1,2 +1,24 @@
-package alura.com.api.domain.respuesta;public class Respuesta {
+package alura.com.api.domain.respuesta;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity(name = "Respuesta")
+@Table(name = "respuestas")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@EqualsAndHashCode(of = "id")
+public class Respuesta {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String mensaje;
+  private Long topico_id;
+  private LocalDate fecha_creacion;
+  private Long usuario_id;
+  private String solucion;
 }
