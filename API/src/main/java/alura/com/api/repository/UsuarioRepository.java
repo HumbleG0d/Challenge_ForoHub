d5481e6ac19dbd8bac5671ffd5997a6b9cc44bb4
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-07-08 16:18:32 -0500

    Add repositorio topico

--- API/src/main/java/alura/com/api/repository/UsuarioRepository.java
+++ API/src/main/java/alura/com/api/repository/UsuarioRepository.java
@@ -1,7 +1,12 @@
 package alura.com.api.repository;
 
+import alura.com.api.domain.usuario.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public class UserRespository {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+  Optional<Usuario> findByNombre(String username);
 }
