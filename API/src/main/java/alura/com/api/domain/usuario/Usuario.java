e60d0a75aedc01519a6237f942a99bf232c61118
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-07-12 11:44:48 -0500

    Add the implementation of the UserDetails interface

--- API/src/main/java/alura/com/api/domain/usuario/Usuario.java
+++ API/src/main/java/alura/com/api/domain/usuario/Usuario.java
@@ -2,6 +2,12 @@ package alura.com.api.domain.usuario;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity(name = "Usuario")
 @Table(name = "usuarios")
@@ -10,7 +16,7 @@ import lombok.*;
 @Setter
 @Getter
 @EqualsAndHashCode(of = "id")
-public class Usuario {
+public class Usuario implements UserDetails {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
@@ -27,4 +33,35 @@ public class Usuario {
   public Usuario(DatosUsuarioDTO datosUsuarioDTO) {
     this.nombre = datosUsuarioDTO.nombre();
   }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+  }
+
+  @Override
+  public String getPassword() {
+    return contrasena;
+  }
+
+  @Override
+  public String getUsername() {
+    return nombre;
+  }
+
+  @Override
+  public boolean isAccountNonExpired(){
+    return  true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired(){
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled(){
+    return true;
+  }
+
 }
