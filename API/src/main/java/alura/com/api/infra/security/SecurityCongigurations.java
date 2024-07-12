6bb9a4a25ece33082a7386f6126c49cdffbb559d
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-07-12 11:55:37 -0500

    Add security filter

--- API/src/main/java/alura/com/api/infra/security/SecurityCongigurations.java
+++ API/src/main/java/alura/com/api/infra/security/SecurityCongigurations.java
@@ -1,2 +1,44 @@
-package alura.com.api.infra.security;public class SecurityCongigurations {
+package alura.com.api.infra.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityCongigurations {
+
+  @Autowired
+  private SecurityFilter securityFilter;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    return httpSecurity.csrf(csrf -> csrf.disable())
+            .sessionManagement(sees -> sees.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                    .anyRequest().authenticated()
+            )
+            .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+            .build();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
+
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder(){
+    return new BCryptPasswordEncoder();
+  }
 }
--- API/src/main/java/alura/com/api/infra/security/SecurityFilter.java
+++ API/src/main/java/alura/com/api/infra/security/SecurityFilter.java
@@ -1,2 +1,50 @@
-package alura.com.api.infra.security;public class SecurityFilter {
+package alura.com.api.infra.security;
+import alura.com.api.repository.UsuarioRepository;
+import alura.com.api.services.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+  @Autowired
+  private TokenService tokenService;
+  @Autowired
+  private UsuarioRepository usuarioRepository;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    var authHeader = request.getHeader("Authorization");
+    if (authHeader != null && authHeader.startsWith("Bearer ")) {
+      var token = authHeader.replace("Bearer ", "");
+      System.out.println("Token recibido: " + token);
+      var nombreUsuario = tokenService.getSubject(token);
+      System.out.println("Nombre de usuario del token: " + nombreUsuario);
+      if (nombreUsuario != null) {
+        var usuario = usuarioRepository.findByNombre(nombreUsuario).orElse(null);
+        if (usuario != null) {
+          var authentication = new UsernamePasswordAuthenticationToken(usuario, null,
+                  usuario.getAuthorities());
+          SecurityContextHolder.getContext().setAuthentication(authentication);
+          System.out.println("Usuario autenticado: " + usuario.getNombre());
+        } else {
+          System.out.println("Usuario no encontrado en el repositorio");
+        }
+      } else {
+        System.out.println("Nombre de usuario extraído del token es nulo");
+      }
+    } else {
+      System.out.println("Header de autorización no encontrado o no comienza con 'Bearer '");
+    }
+    filterChain.doFilter(request, response);
+  }
 }
