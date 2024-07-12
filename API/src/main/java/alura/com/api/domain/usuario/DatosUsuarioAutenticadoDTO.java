99d2350b62b6d2fc0436e5424dd7d8a75411de57
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-07-12 11:45:45 -0500

    Add a data record for user authentication

--- API/src/main/java/alura/com/api/domain/usuario/DatosUsuarioAutenticadoDTO.java
+++ API/src/main/java/alura/com/api/domain/usuario/DatosUsuarioAutenticadoDTO.java
@@ -1,2 +1,11 @@
-package alura.com.api.domain.usuario;public record DatosUsuarioAutenticadoDTO() {
+package alura.com.api.domain.usuario;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record DatosUsuarioAutenticadoDTO(
+        @NotBlank
+        String nombre,
+        @NotBlank
+        String contrasena
+) {
 }
