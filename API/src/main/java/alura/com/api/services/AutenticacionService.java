1480aab868cc9cf65e835166e00772aca1e43262
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-07-12 11:51:33 -0500

    Add service for authentication

--- API/src/main/java/alura/com/api/services/AutenticacionService.java
+++ API/src/main/java/alura/com/api/services/AutenticacionService.java
@@ -1,2 +1,21 @@
-package alura.com.api.services;public class AutenticacionService {
+package alura.com.api.services;
+
+import alura.com.api.domain.usuario.Usuario;
+import alura.com.api.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacionService implements UserDetailsService {
+
+  @Autowired
+  private UsuarioRepository usuarioRepository;
+  @Override
+  public UserDetails loadUserByUsername(String nombre) throws UsernameNotFoundException {
+    Usuario usuario = usuarioRepository.findByNombre(nombre).orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado "+ nombre));
+    return usuario;
+  }
 }
